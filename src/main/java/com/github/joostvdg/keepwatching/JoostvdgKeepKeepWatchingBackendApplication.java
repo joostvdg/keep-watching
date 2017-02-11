5d5834bd7d02905b30829fe77c8bb434c35aeb39
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-02-11 15:20:15 +0100

    Inital Commit

---
+++ src/main/java/com/github/joostvdg/keepwatching/JoostvdgKeepKeepWatchingBackendApplication.java
@@ -0,0 +1,12 @@
+package com.github.joostvdg.keepwatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoostvdgKeepKeepWatchingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoostvdgKeepKeepWatchingBackendApplication.class, args);
+	}
+}

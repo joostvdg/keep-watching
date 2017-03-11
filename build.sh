6c7ce2815546e301d38587031e542d1c77f32525
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-03-11 18:39:46 +0100

    By default go to locally hosted postgresql and for build use container

--- build.sh
+++ build.sh
@@ -7,16 +7,13 @@ docker-compose pull
 docker-compose up -d db
 sleep 15
 docker logs keepwatching_db_1
-docker-compose start db
-sleep 5
-docker logs keepwatching_db_1
 echo "## CHECK DB IP"
 DB_IP=$(docker inspect --format '{{.NetworkSettings.Networks.keepwatching_default.IPAddress}}' keepwatching_db_1)
 echo "# IP=${DB_IP}"
 echo "##############"
 echo "##############"
 echo "## RUN MAVEN PACKAGE"
-mvn clean package -P db -Dspring.profiles.active=compose -Dspring.datasource.url=jdbc:postgresql://${DB_IP}:5432/kw -Dspring.datasource.username=docker -Dspring.datasource.password=docker
+mvn clean package -P db -Dspring.profiles.active=compose -Ddb.url=jdbc:postgresql://${DB_IP}:5432/kw -Dspring.datasource.url=jdbc:postgresql://${DB_IP}:5432/kw -Dspring.datasource.username=docker -Dspring.datasource.password=docker
 echo "##############"
 echo "##############"
 echo "## STOP DB"

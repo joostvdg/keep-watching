393c6580653e84e6782b8814a245f38e4c7c6704
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-03-11 17:37:26 +0100

    Use docker compose up to make sure it is all setup

--- build.sh
+++ build.sh
@@ -3,18 +3,17 @@ echo "##############"
 echo "## MAVEN PACKAGE"
 echo "##############"
 echo "## START DB"
-docker-compose create
-docker-compose start db
+docker-compose up -d db
 sleep 10
 echo "## CHECK DB IP"
 DB_IP=$(docker inspect --format '{{.NetworkSettings.Networks.keepwatching_default.IPAddress}}' keepwatching_db_1)
 echo "# IP=${DB_IP}"
 echo "##############"
 echo "##############"
-echo "## RUN MAVEN TESTS"
+echo "## RUN MAVEN PACKAGE"
 mvn clean package -P db -Dspring.profiles.active=compose -Dspring.datasource.url=jdbc:postgresql://${DB_IP}:5432/kw -Dspring.datasource.username=docker -Dspring.datasource.password=docker
 echo "##############"
 echo "##############"
 echo "## STOP DB"
-docker-compose stop db
+docker-compose down
 echo "##############"

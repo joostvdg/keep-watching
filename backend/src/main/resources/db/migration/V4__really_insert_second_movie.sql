c7b991b12f319e34cb78d93437051e16c3aa3d58
Hz Lin <hz.lin0@outlook.com>
2017-03-12 19:42:14 +0100

    Really insert Star Wars
    Insert movie Star wars: the last jedi

---
+++ backend/src/main/resources/db/migration/V4__really_insert_second_movie.sql
@@ -0,0 +1,3 @@
+INSERT INTO Movies
+       (movie_name,producer,director,studio,notable_actors, release_year, release_date, seen, cinema_worthy, genre, want, imdb)
+VALUES ('Star Wars: The Last Jedi', '', 'Rian Johnson', 'Disney', 'Daisy Ridley,John Boyega,Mark Hamill', '2017', '2017-12-15', false, true, 'Action,Drama,Sci-Fi', true, 'http://www.imdb.com/title/tt2527336/' );

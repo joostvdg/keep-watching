6cb1ca9413c6dad24b4ed9e53ad89aa246f83b2d
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-07-25 22:27:57 +0200

    Fix docker for heroku

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # postgres://cjtetdzimdkcyj:2c77dff5cbcc39feda255ad9bde803e4822b7a0ac20880cbf4c33e0a07963908@ec2-50-19-218-160.compute-1.amazonaws.com:5432/d1trtpsrrej15q
+echo "HELLO"
 if [ "${DATABASE_URL}" ]; then
+echo "HELLO=$DATABASE_URL"
     URL=$DATABASE_URL
     INDEX_AT=`expr index "$URL" @`
     LENGTH_PREFIX=11
@@ -13,9 +15,11 @@ if [ "${DATABASE_URL}" ]; then
     #echo "USR_PASS=$USR_PASS"
     #echo "USR=$USR"
     #echo "PSS=$PSS"
-    URL_REMAINDER=${URL:$LENGTH_AT}
+    URL_REMAINDER=${DATABASE_URL:$LENGTH_AT + $LENGTH_PREFIX + 1}
     #echo "URL_REMAINDER=$URL_REMAINDER"
     export JDBC_DATABASE_URL="jdbc:postgresql://$URL_REMAINDER"
+    echo "JDBC_DATABASE_URL=$JDBC_DATABASE_URL"
     export JDBC_DATABASE_USERNAME="$USR"
     export JDBC_DATABASE_PASSWORD="$PSS"
 fi
+exec "$@"
